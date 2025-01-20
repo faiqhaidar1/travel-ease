ed93f35ad39173f5144090a9a7946c9e90cb98bd
faiqhaidar1 <callbackfaiq1@gmail.com>
2025-01-20 22:05:19 +0700

    update

--- resources/views/frontend/travel/checkout.blade.php
+++ resources/views/frontend/travel/checkout.blade.php
@@ -38,34 +38,34 @@ data-client-key="{{ config('midtrans.client_key') }}"></script>
     </table>
     <button class="btn btn-primary" id="pay-button">Booking Sekarang</button>
 </div>
-<div id="snap-container">
-    <script type="text/javascript">
-        // For example trigger on button clicked, or any time you need
-        var payButton = document.getElementById('pay-button');
-        payButton.addEventListener('click', function () {
-          // Trigger snap popup. @TODO: Replace TRANSACTION_TOKEN_HERE with your transaction token.
-          // Also, use the embedId that you defined in the div above, here.
-          window.snap.embed('{{ $snapToken }}', {
-            embedId: 'snap-container',
-            onSuccess: function (result) {
-              /* You may add your own implementation here */
-              alert("payment success!"); console.log(result);
-            },
-            onPending: function (result) {
-              /* You may add your own implementation here */
-              alert("wating your payment!"); console.log(result);
-            },
-            onError: function (result) {
-              /* You may add your own implementation here */
-              alert("payment failed!"); console.log(result);
-            },
-            onClose: function () {
-              /* You may add your own implementation here */
-              alert('you closed the popup without finishing the payment');
-            }
-          });
+<div id="snap-container" class="d-flex justify-content-center mt-4">
+  <script type="text/javascript">
+      // For example trigger on button clicked, or any time you need
+      var payButton = document.getElementById('pay-button');
+      payButton.addEventListener('click', function () {
+        // Trigger snap popup. @TODO: Replace TRANSACTION_TOKEN_HERE with your transaction token.
+        // Also, use the embedId that you defined in the div above, here.
+        window.snap.embed('{{ $snapToken }}', {
+          embedId: 'snap-container',
+          onSuccess: function (result) {
+            /* You may add your own implementation here */
+            alert("payment success!"); console.log(result);
+          },
+          onPending: function (result) {
+            /* You may add your own implementation here */
+            alert("waiting your payment!"); console.log(result);
+          },
+          onError: function (result) {
+            /* You may add your own implementation here */
+            alert("payment failed!"); console.log(result);
+          },
+          onClose: function () {
+            /* You may add your own implementation here */
+            alert('you closed the popup without finishing the payment');
+          }
         });
-      </script>
+      });
+  </script>
 </div>
 
 @endsection
